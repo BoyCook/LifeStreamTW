c528628f4c3fc3de75503219abf6993ea45832b5
Craig Cook <boycook@me.com>
2013-07-24 09:31:53 +0100

    Added static dir

--- tiddlywebconfig.py
+++ tiddlywebconfig.py
@@ -2,8 +2,10 @@
 # `pydoc tiddlyweb.config` for details on configuration items.
 
 config = {
-    'system_plugins': ['lifestream'],
+    'system_plugins': ['lifestream', 'tiddlywebplugins.static'],
     'secret': '150d9eabe2bd925c536ef4451afe3c345cebdb2b',
     'twanager_plugins': ['tiddlywebwiki'],
+	'static_url_dir': 'static',
+	'static_file_dir': './static',    
     'log_level': 'DEBUG',    
 }
