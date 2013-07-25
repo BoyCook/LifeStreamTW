a64247768922ba61d2d3ba9ce51691db0145b8f5
Craig Cook <boycook@me.com>
2013-07-25 10:44:18 +0100

    Added in wordpress client

--- tiddlywebplugins/Loader.py
+++ tiddlywebplugins/Loader.py
@@ -1,22 +1,29 @@
-from twython import Twython
 from tiddlyweb.model.tiddler import Tiddler
 from config import config
+from twython import Twython
+from wordpress import WordPress
 
 
 class Loader():
     def __init__(self, store):
         self.store = store
+        self.twitter = Twython(config['app_key'], config['app_secret'], config['oauth_token'], config['oauth_token_secret'])
+        self.wp = WordPress('', cache=None)
 
     def load_tweets(self):
         # https://github.com/ryanmcgrath/twython
         print 'Loading tweets...'
-        twitter = Twython(config['app_key'], config['app_secret'], config['oauth_token'], config['oauth_token_secret'])
-        tweets = twitter.get_user_timeline()
+
+        tweets = self.twitter.get_user_timeline()
         for tweet in tweets:
-            self.add_tiddler(tweet)
+            self.add_tweet_tiddler(tweet)
         return tweets
 
-    def add_tiddler(self, tweet):
+    def load_blog_posts(self):
+        # https://github.com/eyeseast/python-wordpress
+        self.wp.get_recent_posts()
+
+    def add_tweet_tiddler(self, tweet):
         id_str = tweet['id_str']
         tiddler = Tiddler('Tweet' + id_str, 'tweets')
         tiddler.text = tweet['text']
