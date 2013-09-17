514cd3bf3f699ef81a9bcec9d1850a7471ccd628
Craig Cook <boycook@me.com>
2013-09-17 11:58:43 +0100

    Added coverage support

--- test/test_loader.py
+++ test/test_loader.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
 import unittest
+from tiddlyweb.store import Store
+from tiddlyweb.config import config
 from tiddlywebplugins.Loader import Loader
+
 
 class LoaderTest(unittest.TestCase):
     def setUp(self):
-        self.loader = Loader(None)
+        store = Store(config['server_store'][0],
+                      config['server_store'][1],
+                      environ={'tiddlyweb.config': config})
+        self.loader = Loader(store)
 
     def test_load_tweets(self):
         self.loader.load_tweets()
@@ -15,6 +21,7 @@ class LoaderTest(unittest.TestCase):
 
     def test_load_tweets(self):
         self.loader.load_github()
+
 
 if __name__ == '__main__':
     unittest.main()
