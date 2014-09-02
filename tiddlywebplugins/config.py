ad31c5b11f222bd1ed81ae1c9424368d941021e0
Craig Cook <boycook@me.com>
2014-09-02 22:12:39 +0100

    Fixed duplicate param name

--- tiddlywebplugins/config.py
+++ tiddlywebplugins/config.py
@@ -4,5 +4,5 @@ config = {
     'app_key': os.environ['APP_KEY'],    
     'app_secret': os.environ['APP_SECRET'],
     'oauth_token': os.environ['OAUTH_TOKEN'],
-    'oauth_token_secret': os.environ['APP_KEY']
+    'oauth_token_secret': os.environ['OAUTH_TOKEN_SECRET']
 }
